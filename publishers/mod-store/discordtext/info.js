cbacc8780ba8c7dcc7331ec52d6af116c37f5710
curtis <cjgann28122012@gmail.com>
2025-05-13 14:51:15 -0500

    Add files via upload

---
+++ publishers/mod-store/discordtext/info.js
@@ -0,0 +1,15 @@
+const appInfo = {
+  name: "Discord Text",
+  version: "1.0",
+  publisher: "mod-store",
+  size: "50 MB",
+  description: "A simple app to send text in Discord.",
+  features: "HI",
+  icon: "/publishers/mod-store/discordtext/logo.png",
+  screenshots: [
+    "/publishers/mod-store/discordtext/img1.png",
+    "/publishers/mod-store/discordtext/img2.png",
+    "/publishers/mod-store/discordtext/img3.png",
+  ],
+  downloadLink: "/publishers/mod-store/discordtext/download.apk"
+};
